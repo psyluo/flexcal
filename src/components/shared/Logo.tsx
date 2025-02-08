fbd9e6b5efd53a420fcaad4cf413795c6a334a21
psyluo <erinluoyi@gmail.com>
2025-02-08 19:17:00 +0800

    Replace image logo with text logo

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
-import logoImage from '../../assets/logo.png';
 
 interface LogoProps {
   size?: number;
 }
 
-const LogoImg = styled.img<LogoProps>`
-  width: ${props => props.size || 24}px;
-  height: ${props => props.size || 24}px;
-  object-fit: contain;  // 保持宽高比
+const LogoText = styled.div<LogoProps>`
+  font-size: ${props => props.size ? `${props.size/2}px` : '32px'};
+  font-weight: bold;
+  color: #7c3aed;
 `;
 
 export const Logo: React.FC<LogoProps> = ({ size = 24 }) => {
   return (
-    <LogoImg 
-      src={logoImage}
-      size={64}
-      alt="FlexCal Logo"
-    />
+    <LogoText size={size}>
+      FC
+    </LogoText>
   );
 }; 
